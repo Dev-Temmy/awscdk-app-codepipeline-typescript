1d15c28943e562a5ac0eb17ce260ebe470ace00b
Dev-Temmy <callonyemmo@gmail.com>
2022-06-16 15:44:20 +1000

    initial commit

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,17 +1,30 @@
 import * as cdk from '@aws-cdk/core';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
+import { CodePipeline, CodePipelineSource, CodeBuildStep } from '@aws-cdk/pipelines';
 
 export class CDKPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Set your Github username and repository name
+        const branch = 'master';
+        const gitHubUsernameRepository = 'Dev-Temmy/awscdk-app-codepipeline-typescript';
+
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyCDKPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('Dev-Temmy/awscdk-app-codepipeline-typescript', 'master'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
-    });
+      synth: new CodeBuildStep('SynthStep', {
+        input: CodePipelineSource.gitHub(gitHubUsernameRepository, branch, {
+          authentication: cdk.SecretValue.secretsManager('Github-awscdk-app-codepipeline-typescript-token'),
+    }),
+      installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            })
+        });
+    }
   }
-}
